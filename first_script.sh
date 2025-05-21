e217c5c51fa88b6219de3a8fdb701e1832a0783b
root <root@DESKTOP-9K1QG7E.>
2025-05-21 12:00:38 +0300

    These are files for my class activities

---
+++ first_script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+Name=Diana
+
+echo "$Name"
+math=$((2+1))
+echo "$math"
+name="Ndinelao"
+echo "hello, $name!"
+
+# A simple script to greet the user
+
+echo "what is your name"
+
+read name
+
+echo "Hello, $name! Welcome to shell scripting."
